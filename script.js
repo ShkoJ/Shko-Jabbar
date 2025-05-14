96a2c1ec744271cbc5c0c548d12d745ccb7bc327
Shko Jabbar <shkobu420@gmail.com>
2025-05-14 09:34:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
 console.log("Website loaded successfully!");
-function openModal() {
-    document.getElementById("demoModal").style.display = "block";
-}
-
-function closeModal() {
-    document.getElementById("demoModal").style.display = "none";
-}
-
-// Optional: Close the modal when user clicks outside of it
-window.onclick = function(event) {
-    const modal = document.getElementById("demoModal");
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-};
 
