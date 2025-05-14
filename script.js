9344a135dd0dbd7aa121a0841850e837efe22f37
Shko Jabbar <shkobu420@gmail.com>
2025-05-14 09:21:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Website loaded successfully!");
+function openModal() {
+    document.getElementById("demoModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("demoModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById("demoModal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
